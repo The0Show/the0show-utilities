29c9760173f4273003a15ea63ddc9e23c3834bca
The0Show <62229104+The0Show@users.noreply.github.com>
2020-11-20 10:33:51 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,4 @@
 async function theultimatepingcommand(message, args, client) {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 }
